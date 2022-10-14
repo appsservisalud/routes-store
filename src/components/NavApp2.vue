ce873720d8dfed160746e78d52a61ef592fefe3f
appsservisalud <108880293+appsservisalud@users.noreply.github.com>
2022-10-14 15:24:00 -0500

    ok

--- src/components/NavApp2.vue
+++ src/components/NavApp2.vue
@@ -2,5 +2,6 @@
   <ul>
     <li><router-link to="/cuentas">Calculadoras medicas</router-link></li>
     <li><router-link to="/pyp"></router-link>Vacunación</li>
+    <li><router-link to="/pyp"></router-link>Vacunación</li>
   </ul>
 </template>
